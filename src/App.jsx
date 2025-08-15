48fc880202f522b8dc3338dcd309b0c609877c7d
pericarney00 <pericarney@gmail.com>
2025-08-14 20:00:31 -0500

    Built TrackList component and rendered the index of tracks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import * as trackService from "./services/trackService"
+import TrackList from "./components/TrackList/TrackList"
 
 
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [tracks,setTracks] = useState([])
+
+  useEffect(() => {
+    const fetchTracks = async () => {
+      try {
+        const fetchedTracks = await trackService.index()
+        if (fetchedTracks.error) {
+          throw new Error(fetchedTracks.error)
+        }
+        setTracks(fetchedTracks)
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    fetchTracks()
+  }, []);
+
+
+
+
+
+
+  return (
+    <>
+    <h1>JukeBox</h1>
+      <TrackList tracks={tracks} />
+    
+    </>
+  )
 };
 
 
